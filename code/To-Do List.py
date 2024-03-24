71c17af6fe7d501ad4446010dc4dfc19654449db
m-vijju <161595192+m-vijju@users.noreply.github.com>
2024-03-24 16:29:32 +0530

    Update To-Do List.py

--- code/To-Do List.py
+++ code/To-Do List.py
@@ -2,14 +2,14 @@ import tkinter as tk
 from tkinter import messagebox
 
 def default(task):
-    if task_entry.get()=="Enter your task here":
+    if task_entry.get()=="Enter the task here":
         task_entry.delete(0,"end")
-        task_entry.config(fg="black")
+        task_entry.config(fg="red")
 
 def event(task):
     if task_entry.get()=="":
-        task_entry.insert(0,"Enter your task here")
-        task_entry.config(fg="grey")
+        task_entry.insert(0,"Enter the task here")
+        task_entry.config(fg="blue")
         
 
 def add_task():
@@ -32,8 +32,8 @@ root = tk.Tk()
 root.title("To-Do List")
 
 #creting a temporary text in textbox as message
-task_entry=tk.Entry(root,fg="grey",width=30)
-task_entry.insert(0,"Enter your task here")
+task_entry=tk.Entry(root,fg="blue",width=30)
+task_entry.insert(0,"Enter the task here")
 task_entry.bind('<FocusIn>',default)
 task_entry.bind('<FocusOut>',event)
 task_entry.pack(pady=10)
